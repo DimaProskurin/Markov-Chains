29a85ae2da58be43f1e528d1ed5b98223fdd93e9
Dima <dimapr117@gmail.com>
2018-04-06 22:47:19 +0300

    issues исправлены

--- generate.py
+++ generate.py
@@ -2,43 +2,37 @@ import pickle
 import random
 import argparse
 import numpy
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, help='Путь до файла, где лежит частотная модель')
 parser.add_argument('--length', type=int, help='Длина генерируемой последовательности')
 parser.add_argument('--seed', action='store', default='random_seed', help='Начальное слово')
-parser.add_argument('--output', type=str, default='stdout', help='Путь до файла, в который будет записан результат')
+parser.add_argument('--output', type=str, default=None, help='Путь до файла, в который будет записан результат')
+args = parser.parse_args()
 
-args = vars(parser.parse_args())
-
-with open(args['model'], 'rb') as f:
+with open(args.model, 'rb') as f:
     dictionary = pickle.load(f)
 
-sentence = ""
-if args['seed'] == 'random_seed':
+if args.output != None:
+    sys.stdout = open(args.output, 'w')
+
+if args.seed == 'random_seed':
     seed = random.choice(list(dictionary.keys()))
 else:
-    seed = args['seed']
+    seed = args.seed
 
-sentence += seed + ' '
-args['length'] -= 1
+print(seed, end=' ')
+args.length -= 1
 
 current_word = seed
 next_word = ''
 
-while args['length'] > 0:
-    sum = 0
-    for key in dictionary[current_word].keys():
-        sum += dictionary[current_word][key]
-    probability = [dictionary[current_word][key]/sum for key in dictionary[current_word].keys()]
+while args.length > 0:
+    s = sum(dictionary[current_word].values())
+    probability = [item / s for item in dictionary[current_word].values()]
 
-    next_word = numpy.random.choice(list(dictionary[current_word].keys()), None, p=probability)
-    sentence += next_word + ' '
-    args['length'] -= 1
+    next_word = numpy.random.choice(list(dictionary[current_word].keys()), p=probability)
+    print(next_word, end=' ')
+    args.length -= 1
     current_word = next_word
-
-if (args['output'] == 'stdout'):
-    print(sentence)
-else:
-    with open(args['output'], 'w') as f:
-        f.write(sentence)
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import re
 import os
 import pickle
 import argparse
+import sys
 from collections import defaultdict
 
 r_alphabet = re.compile(u'[а-яА-Яa-zA-Z]+')
@@ -10,26 +11,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input-dir', type=str, default='stdin', help='Путь к папке с файлами для БД текстов')
 parser.add_argument('--model', type=str, help='Путь до файла, в который будет сохраняться частотная модель')
 parser.add_argument('--lc', action='store_true', default=False, help='Приводит тексты к нижнему регистру')
-
-args = vars(parser.parse_args())
-input_dir = args['input_dir']
-model_dir = args['model']
-low_case = args['lc']
+args = parser.parse_args()
 
 
 def gen_lines(file):
     with open(file, 'r') as the_file:
         for line in the_file:
-            if low_case:
-                yield line.lower()
-            else:
-                yield line
+            yield line
 
 
 def gen_tokens(lines):
     for line in lines:
-        for token in r_alphabet.findall(line):
-            yield token
+        if args.lc:
+            for token in r_alphabet.findall(line.lower()):
+                yield token
+        else:
+            for token in r_alphabet.findall(line):
+                yield token
 
 
 def make_dictionary(tokens):
@@ -45,21 +43,13 @@ def make_dictionary(tokens):
 
 
 dictionary = {}
-if input_dir == 'stdin':
-    # здесь я вынужден ограничить пользователя вводом только одной строки,
-    # так как иначе непонятно, когда он закончил ввод своего текста
-    line = input()
-    if low_case:
-        tokens = r_alphabet.findall(line.lower())
-    else:
-        tokens = r_alphabet.findall(line)
-
-    dictionary = make_dictionary(tokens)
+if args.input_dir == 'stdin':
+    dictionary = make_dictionary(gen_tokens(sys.stdin))
 else:
-    lst_of_files = os.listdir(input_dir)
+    lst_of_files = os.listdir(args.input_dir)
     for file in lst_of_files:
-        tmp_dictionary = make_dictionary(gen_tokens(gen_lines(os.path.join(input_dir, file))))
+        tmp_dictionary = make_dictionary(gen_tokens(gen_lines(os.path.join(args.input_dir, file))))
         dictionary = {**dictionary, **tmp_dictionary}
 
-with open(model_dir, 'wb') as f:
+with open(args.model, 'wb') as f:
     pickle.dump(dictionary, f)
