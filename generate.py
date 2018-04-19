435b70b9d29a98bf0ffb9c37d67f1aaa5d1e5abc
Dima <dimapr117@gmail.com>
2018-04-19 23:47:04 +0300

    3 issues are fixed

--- generate.py
+++ generate.py
@@ -4,46 +4,46 @@ import argparse
 import numpy
 import sys
 
-'''Парсер аргументов консоли'''
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str,
-                    help='Путь до файла, где лежит частотная модель')
-parser.add_argument('--length', type=int,
-                    help='Длина генерируемой последовательности')
-parser.add_argument('--seed', action='store', default='random_seed',
-                    help='Начальное слово')
-parser.add_argument('--output', type=str, default=None,
-                    help='Путь до файла, в который будет записан результат')
-args = parser.parse_args()
+if __name__ == "__main__":
+    '''Парсер аргументов консоли'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str,
+                        help='Путь до файла, где лежит частотная модель')
+    parser.add_argument('--length', type=int,
+                        help='Длина генерируемой последовательности')
+    parser.add_argument('--seed', action='store', default='random_seed',
+                        help='Начальное слово')
+    parser.add_argument('--output', type=str, default=None,
+                        help='Путь до файла, где будет записан результат')
+    args = parser.parse_args()
 
-'''Загружаем частотную модель'''
-with open(args.model, 'rb') as f:
-    dictionary = pickle.load(f)
+    '''Загружаем частотную модель'''
+    with open(args.model, 'rb') as f:
+        dictionary = pickle.load(f)
 
-'''Открываем файл для записи результата(опционально)'''
-if args.output is not None:
-    sys.stdout = open(args.output, 'w')
+    '''Открываем файл для записи результата(опционально)'''
+    if args.output is not None:
+        sys.stdout = open(args.output, 'w')
 
-'''Выбор начального слова, генерируемой последовательности'''
-if args.seed == 'random_seed':
-    seed = random.choice(list(dictionary.keys()))
-else:
-    seed = args.seed
+    '''Выбор начального слова, генерируемой последовательности'''
+    if args.seed == 'random_seed':
+        seed = random.choice(list(dictionary.keys()))
+    else:
+        seed = args.seed
 
-print(seed, end=' ')
-args.length -= 1
-
-current_word = seed
-next_word = ''
-
-'''Создание и вывод генерируемой последовательности'''
-while args.length > 0:
-    s = sum(dictionary[current_word].values())
-    probability = [item / s for item in dictionary[current_word].values()]
-
-    next_word = numpy.random.choice(
-        list(dictionary[current_word].keys()),
-        p=probability)
-    print(next_word, end=' ')
+    print(seed, end=' ')
     args.length -= 1
-    current_word = next_word
+
+    current_word = seed
+    next_word = ''
+
+    '''Создание и вывод генерируемой последовательности'''
+    for i in range(args.length):
+        s = sum(dictionary[current_word].values())
+        probability = [item / s for item in dictionary[current_word].values()]
+
+        next_word = numpy.random.choice(
+            list(dictionary[current_word].keys()),
+            p=probability)
+        print(next_word, end=' ')
+        current_word = next_word
--- train.py
+++ train.py
@@ -5,24 +5,27 @@ import argparse
 import sys
 from collections import defaultdict
 
+if __name__ == "__main__":
+    '''Это регулярные выражения, \
+    они нужны, чтобы оставить в тексте только слова'''
+    r_alphabet = re.compile(u'[а-яА-Яa-zA-Z]+')
 
-'''Это регулярные выражения, они нужны, чтобы оставить в тексте только слова'''
-r_alphabet = re.compile(u'[а-яА-Яa-zA-Z]+')
-
-'''Парсер аргументов консоли'''
-parser = argparse.ArgumentParser()
-parser.add_argument('--input-dir', type=str, default='stdin',
-                    help='Путь к папке с файлами для БД текстов')
-parser.add_argument('--model', type=str,
-                    help='Путь до файла, где сохраняется частотная модель')
-parser.add_argument('--lc', action='store_true', default=False,
-                    help='Приводит тексты к нижнему регистру')
-args = parser.parse_args()
+    '''Парсер аргументов консоли'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-dir', type=str, default='stdin',
+                        help='Путь к папке с файлами для БД текстов')
+    parser.add_argument('--model', type=str,
+                        help='Путь до файла, где сохраняется частотная модель')
+    parser.add_argument('--lc', action='store_true', default=False,
+                        help='Приводит тексты к нижнему регистру')
+    args = parser.parse_args()
 
 
 def gen_lines(file):
     '''
     Генератор строк по файлу
+    Arguments: file (type str) - путь к файлу
+    Return: line (type str) - строка
     '''
     with open(file, 'r') as the_file:
         for line in the_file:
@@ -32,6 +35,8 @@ def gen_lines(file):
 def gen_tokens(lines):
     '''
     Генератор слов по даваемому генератору строк
+    Arguments: lines (type generator) - генератор строк
+    Return: token (type str) - слово
     '''
     for line in lines:
         if args.lc:
@@ -45,6 +50,8 @@ def gen_tokens(lines):
 def make_dictionary(tokens):
     '''
     Создатель частотного словаря по генератору слов
+    Arguments: tokens (type generator) - генератор слов
+    Return: dictionary (type dict) - частотный словарь
     '''
     dictionary = dict()
     lst = list(tokens)
@@ -68,7 +75,7 @@ else:
             os.path.join(args.input_dir, file))))
         dictionary = {**dictionary, **tmp_dictionary}
 
-
-'''Сохранение частотной модели в файл'''
-with open(args.model, 'wb') as f:
-    pickle.dump(dictionary, f)
+if __name__ == "__main__":
+    '''Сохранение частотной модели в файл'''
+    with open(args.model, 'wb') as f:
+        pickle.dump(dictionary, f)
